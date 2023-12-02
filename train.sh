07baea0fd39ed89e1447d4c3fd52207d14fc35c1
Johnathan Tran <j4tran@ucsd.edu>
2023-12-01 22:06:00 -0800

    Finding ideal batch size

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 lang=python #programming language
 lr=5e-5
-batch_size=64
+batch_size=32
 beam_size=10
 source_length=256
 target_length=128
@@ -10,8 +10,8 @@ train_file=$data_dir/python-cleaned-train.jsonl
 dev_file=$data_dir/python-cleaned-val.jsonl
 # eval_steps=400 #400 for ruby, 600 for javascript, 1000 for others
 # train_steps=20000 #20000 for ruby, 30000 for javascript, 50000 for others
-eval_steps=60 #400 for ruby, 600 for javascript, 1000 for others
-train_steps=3000 #20000 for ruby, 30000 for javascript, 50000 for others
+eval_steps=120 #400 for ruby, 600 for javascript, 1000 for others
+train_steps=6000 #20000 for ruby, 30000 for javascript, 50000 for others
 pretrained_model=microsoft/codebert-base #Roberta: roberta-base
 test_model=pytorch_model.bin #checkpoint for test
 
